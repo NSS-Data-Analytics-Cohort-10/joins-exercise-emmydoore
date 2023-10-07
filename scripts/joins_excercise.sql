2fde80fbb4d38af18c5fc064b01e1c61ee3be6e4
emmydoore <emmaleidoore@gmail.com>
2023-10-07 14:03:20 -0500

    all done

--- scripts/joins_excercise.sql
+++ scripts/joins_excercise.sql
@@ -40,13 +40,45 @@ from distributors as d
 left join specs as s
 on d.distributor_id = s.domestic_distributor_id
 group by d.company_name;
---Answer
 
 -- 5. Write a query that returns the five distributors with the highest average movie budget.
---Answer
+select avg(r.film_budget) as avg_film_budget,
+		d.company_name		
+from specs as s
+inner join distributors as d
+on s.domestic_distributor_id = d.distributor_id
+inner join revenue as r
+on s.movie_id = r.movie_id
+group by distinct d.company_name
+order by avg(r.film_budget) desc
+limit 5; 
+--Answer: Walt Disney, Sony Pictures, Lionsgate, DreamWorks, Warner Bros.
 
 -- 6. How many movies in the dataset are distributed by a company which is not headquartered in California? Which of these movies has the highest imdb rating?
---Answer
+select count(s.film_title)
+from specs as s
+inner join distributors as d
+on s.domestic_distributor_id = d.distributor_id
+where d.headquarters not ilike '%%%CA';
+--Answer: 2
+
+select  s.film_title,
+		r.imdb_rating
+from specs as s
+inner join rating as r
+using (movie_id)
+inner join distributors as d
+on s.domestic_distributor_id = d.distributor_id
+where d.headquarters not ilike '%%%CA'
+group by s.film_title, r.imdb_rating
+order by r.imdb_rating desc;
+--Answer: Dirty Dancing, 7.0
+
 
 -- 7. Which have a higher average rating, movies which are over two hours long or movies which are under two hours?
---Answer
+select avg(imdb_rating) as avg_imdb_rating, length_in_min>=120 as over_120_mins, length_in_min<120 as under_120_mins
+from rating
+inner join specs
+using (movie_id)
+group by length_in_min>=120,length_in_min<120;
+--Answer: Movies under 120 minutes/2 hours
